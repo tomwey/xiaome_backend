946a8b631a688f29639e6ed24ea7a1d6c9a83a92
tomwey <tomwey@163.com>
2018-09-27 17:57:04 +0800

    ccc

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,7 +4,7 @@ require 'redis/objects'
 
 redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 
-$redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
+$redis = Redis.new(host: redis_config['host'], port: redis_config['port'], password: SiteConfig.redis_pass)
 Redis::Objects.redis = $redis
 
 sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/1"
