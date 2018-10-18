0583f72d5ca3da4d599a4ba21792c78b7e7fa2b1
tomwey <tomwey@163.com>
2018-10-18 16:56:40 +0800

    ccc

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   validates :name, :sex, :idcard, presence: true
+  validates :idcard, format: { with: /\A\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|[xX])\z/, message: '不正确的身份证号码' }
 end
