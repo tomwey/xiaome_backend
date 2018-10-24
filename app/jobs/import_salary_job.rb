dace29d7b4249b9858019d36be7273b2575d8436
tomwey <tomwey@163.com>
2018-10-24 16:18:29 +0800

    ccc

--- app/jobs/import_salary_job.rb
+++ app/jobs/import_salary_job.rb
@@ -5,7 +5,9 @@ class ImportSalaryJob < ActiveJob::Base
 
   def perform(row)
     user_ids = Profile.where(phone: row['phone'], name: row['name']).pluck(:user_id)
+    puts user_ids
     @salaries = Salary.where(user_id: user_ids, payed_at: nil, money: row['money'].to_f).all
+    puts @salaries
     if @salaries.any?
       @salaries.map { |salary| salary.update(state: 'approved') }
     end
