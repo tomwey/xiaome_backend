c36c7f4b4aaae4bc703215c3f61524a310b0ec16
tomwey <tomwey@163.com>
2018-10-24 16:22:45 +0800

    ccc

--- app/jobs/import_salary_job.rb
+++ app/jobs/import_salary_job.rb
@@ -4,6 +4,7 @@ class ImportSalaryJob < ActiveJob::Base
   queue_as :scheduled_jobs
 
   def perform(row)
+    puts row
     user_ids = Profile.where(phone: row['phone'], name: row['name']).pluck(:user_id)
     puts user_ids
     @salaries = Salary.where(user_id: user_ids, payed_at: nil, money: row['money'].to_f).all
